30a93d6d0aef398f36d2661f66e247e8c57b1f36
Mihaita Leoveanu <playerunu@gmail.com>
2021-09-22 20:29:15 +0300

    Renamed challenge status JSON serialized field

--- model/Challenge.go
+++ model/Challenge.go
@@ -2,7 +2,7 @@ package models
 
 type Challenge struct {
 	ChallengedBy  *Player `json:"challengedBy,omitempty"`
-	Success       *bool   `json:"challengeStatus,omitempty"`
+	Success       *bool   `json:"success,omitempty"`
 	WaitingReveal bool    `json:"waitingReveal,omitempty"`
 }
 
